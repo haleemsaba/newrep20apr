30bbe82b1abe106593b4ffbe81d1ab5ababdf798
haleemsaba <haleemsaba@hotmail.com>
2016-04-20 12:40:00 +0530

    First Commit

---
+++ WebAppGitDemo/Models/Customer.cs
@@ -0,0 +1,21 @@
+namespace WebAppGitDemo.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Customer : DbContext
+    {
+        public Customer()
+            : base("name=Customer")
+        {
+        }
+
+        public virtual DbSet<CustomerDetail> CustomerDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ WebAppGitDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppGitDemo.Startup))]
+namespace WebAppGitDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
